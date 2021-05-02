7d4e9e9af27c4dc40aa9a3b996c2e45c863a33d3
greyes97 <greyesl1@miumg.edu.gt>
2021-05-01 19:42:31 -0600

    First commit added

---
+++ src/main/java/gt/edu/umg/ingenieria/sistemas/ingenieria_software/lab2/developCalc/DevelopCalcApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.ingenieria_software.lab2.developCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gt/edu/umg/ingenieria/sistemas/ingenieria_software/lab2/developCalc/DevelopCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.ingenieria_software.lab2.developCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
